16973365a1be627e7e3c9d4bdae87452225052d8
bannu-code-star <nitinchiliveri007@gmail.com>
2021-09-28 11:15:28 +0530

    [Naveen] Added a code

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TicTacToe");
+            TicTocToe ticToc = new TicTacToe();
+            ticTac.CreatingBoard();
+        }
+    }
+}
